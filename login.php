d2c6bc0c87193e4205a82deddf492d8b2a697eaf
Jose <perez.jose.132887@gmail.com>
2024-08-19 10:22:19 -0400

    Termine de hacer el login con html

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row align-middle">
+            <div class="col-4 mx-auto formulario" style="margin-top: 100px;">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Correo electronico</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 d-grid gap-2 col-8 mx-auto">
+                        <button class="btn btn-primary" type="submit">INGRESAR</button>
+                    </div>
+                    <div class="mb-3 registrarmecaja">
+                        <a href="registrarme.php" class="registrarme">No tengo una cuenta</a>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
